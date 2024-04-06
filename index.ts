8866abcd712c90b128a768f18bda221b6e278048
Ben Thomas <benjamint08@gmail.com>
2024-04-06 23:39:28 +0100

    Missing a comma

--- index.ts
+++ index.ts
@@ -8,11 +8,11 @@ const server = new ProBun({
     logger: true,
     // mongoUri: Bun.env.MONGO_URI || "mongodb://localhost:27017/", (Optional)
     // pgConfig: {
-    //     user: Bun.env.PG_USER || 'postgres'
+    //     user: Bun.env.PG_USER || 'postgres',
     //     host: Bun.env.PG_HOST || 'localhost',
     //     database: Bun.env.PG_DB || 'postgres',
     //     password: Bun.env.PG_PASSWORD || 'postgres', //replace with actual password
-    //     port: 5432,
+    //     port: Bun.env.PG_PORT || 5432,
     // } (Optional)
 });
 
